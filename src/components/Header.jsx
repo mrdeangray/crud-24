b67b5a4c20819a754e3523763958eda7fe875084
mrdeangray <mrdeangray@gmail.com>
2024-06-01 14:40:48 -0400

    m

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
 
 const Header = ({ className }) => {
+  const { currUser } = useContext(AuthContext);
   return (
     <nav className={className}>
       <h3>CRUD-24</h3>
@@ -9,7 +11,7 @@ const Header = ({ className }) => {
         <li>Link1</li>
         <li>Link1</li>
       </ul>
-      <button>Sign Out</button>
+      <button>{currUser ? currUser.displayName : "Sign Out"}</button>
     </nav>
   );
 };
