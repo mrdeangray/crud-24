0bf1fab49397ce063e0f6b73e30938b2a23be940
mrdeangray <mrdeangray@gmail.com>
2024-06-01 14:56:55 -0400

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
 
 const Home = () => {
-  return <div>
-    <h3>Home</h3>
-    
-  </div>;
+  const { currUser, handleSignIn, handleSignOut } = useContext(AuthContext);
+  return (
+    <div>
+      <h3>Home</h3>
+      {currUser ? (
+        <div>
+          <button onCLick={handleSignOut}>Sign Out</button>
+        </div>
+      ) : (
+        <button onClick={handleSignIn}>Sign In</button>
+      )}
+    </div>
+  );
 };
 
 export default Home;
